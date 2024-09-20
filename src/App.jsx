3730119b690918baa298e1bf927b9cf1d77ea442
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-20 18:50:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 import { useStateContext } from "./context";
+import { Home, Profile, Onboarding } from "./pages";
 
 
 const App = ()=>{
