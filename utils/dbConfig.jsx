75c0b60b698109124ab0df4fddf69aed6cb53eed
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-20 14:40:51 +0530

    Delete utils directory

--- utils/dbConfig.jsx
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-const sql = neon(
-    "postgresql://neondb_owner:6kpD1nCfQHjR@ep-withered-mode-a1aum754.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-);
-export const db = drizzle(sql, { schema });
